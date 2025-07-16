d68ffec0fb46c77a5590732aec8664b67f45213e
Juan carlos IGLESIAS GONZALEZ <jiglesia@f6r7s7.clusters.42paris.fr>
2025-07-16 19:31:35 +0200

    over 95% coverage

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+
+// Only run bootstrap if this file is executed directly (not imported)
+if (require.main === module) {
+  bootstrap();
+}
